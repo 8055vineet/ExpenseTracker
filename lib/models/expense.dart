939a10dfeb6e756a0b5ddd80833935dae8ec5521
8055vineet <patelvineet21u02020@gmail.com>
2023-09-19 18:53:11 +0530

    using enum for specific values

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,10 +1,11 @@
 import 'package:uuid/uuid.dart';
 const uuid=Uuid();
+enum Category {travel,food,work,leisure};
 class expense{
-  expense({required this.title,required this.amount,required this.date}):id=uuid.v4();
+  expense({required this.title,required this.amount,required this.date,required this.category}):id=uuid.v4();
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-
+  final Category category;
 }
