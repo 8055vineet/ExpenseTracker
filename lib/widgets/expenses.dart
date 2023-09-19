9a964c78348c36fd8451b013952db23d2ac9b011
8055vineet <patelvineet21u02020@gmail.com>
2023-09-19 22:21:12 +0530

    adding app bar and + button

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:expensetracker/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expensetracker/widgets/expenses_list/expenses_list.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -9,21 +10,37 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-  final  List<Expense>_registeredExpenses=[
-    Expense(title: "clothes", amount: 1500, date: DateTime.now(), category:Category.work),
-    Expense(title: "cricket", amount: 2300, date: DateTime.now(), category:Category.leisure),
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: "clothes",
+        amount: 1500,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: "cricket",
+        amount: 2300,
+        date: DateTime.now(),
+        category: Category.leisure),
   ];
 
-
   @override
-  Widget build( context) {
-    return  Scaffold(body:
-    Column(
-      children: [
-        const Text("The chart"),
-        Expanded(child: ExpensesList(expenses: _registeredExpenses)),
-      ],
-    ));
+  Widget build(context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Expense Tracker'),
+
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+        ],
+    ),
+        body: Column(
+          children: [
+            const Text("The chart"),
+            Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          ],
+        ));
   }
 }
-
