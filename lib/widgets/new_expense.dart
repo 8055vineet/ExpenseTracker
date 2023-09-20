9d305afaa85c3ce68ec469eca14229ceeb4280f1
8055vineet <patelvineet21u02020@gmail.com>
2023-09-20 22:13:56 +0530

    another method for taking input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,19 +10,19 @@ class NewExpense extends StatefulWidget {
 
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
 
-  void _saveTitleInput(String input) {
-    _enteredTitle = input;
-  }
-
+  final _titleController=TextEditingController();
+ void dispose(){
+   _titleController.dispose();
+   super.dispose();
+ }
   @override
   Widget build(context) {
     return  Padding(
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          TextField(onChanged: _saveTitleInput,
+          TextField(controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
                 label: Text(
@@ -32,7 +32,7 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(children: [
             ElevatedButton(onPressed: () {
-              print(_enteredTitle);
+              print(_titleController.text);
 
             }, child: const Text('save expense'))
           ],),
