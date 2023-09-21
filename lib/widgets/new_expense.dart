22d1389fab470bd539e98087188eb9e24de841b1
8055vineet <patelvineet21u02020@gmail.com>
2023-09-21 20:14:03 +0530

    added option in drop down list

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,6 @@ import 'package:expensetracker/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
@@ -13,22 +12,22 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
-  DateTime?_selectedDate;
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
   void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
 
-    final pickedDate= await showDatePicker(
+    final pickedDate = await showDatePicker(
         context: context,
         initialDate: now,
         firstDate: firstDate,
         lastDate: now);
-    setState((){
-      _selectedDate=pickedDate;
-
+    setState(() {
+      _selectedDate = pickedDate;
     });
   }
-
 
   void dispose() {
     _titleController.dispose();
@@ -73,7 +72,9 @@ class _NewExpenseState extends State<NewExpense> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                   Text(_selectedDate==null?'NO Date Selected':Formatter.format(_selectedDate!)),
+                    Text(_selectedDate == null
+                        ? 'NO Date Selected'
+                        : Formatter.format(_selectedDate!)),
                     IconButton(
                       onPressed: _presentDatePicker,
                       icon: const Icon(Icons.calendar_month),
@@ -83,19 +84,42 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          const SizedBox(
+            height: 15,
+          ),
           Row(
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values.map((category) {
+                    return DropdownMenuItem(
+                      value: category,
+                      child: Text(category.name.toUpperCase()),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    if (value == null)
+                      return;
+                    else {
+                      setState(() {
+                        _selectedCategory = value;
+                      });
+                    }
+                  }),
+              const Spacer(),
               ElevatedButton(
                   onPressed: () {
                     Navigator.pop(context);
                   },
                   child: Text('cancel')),
+              const Spacer(),
               ElevatedButton(
-                  onPressed: () {
-                    print(_amountController.text);
-                    print(_titleController.text);
-                  },
-                  child: const Text('save amount'))
+                onPressed: () {
+                  print(_amountController.text);
+                  print(_titleController.text);
+                },
+                child: const Text('save amount'),
+              ),
             ],
           ),
         ],
